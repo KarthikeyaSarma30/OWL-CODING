02617323067119a8fcfa10b24b60aebb09277d4f
KarthikeyaSarma30 <77091365+KarthikeyaSarma30@users.noreply.github.com>
2022-12-03 12:50:45 +0530

    Add files via upload

---
+++ euclidgcd.py
@@ -0,0 +1,15 @@
+#Euclid's Algorithm(GCD of two numbers)
+#without recursion
+def gcd(a,b):
+    minimum=min(a,b)
+    maximum=max(a,b)
+    a=maximum
+    b=minimum
+    while b:
+        temp=a
+        a=b
+        b=temp%b
+    return a
+print(gcd(48,18))#6
+        
+        

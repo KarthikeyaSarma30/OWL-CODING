e8093ac97dfa54cdfe6d932c55e4d3649cc7d45b
KarthikeyaSarma30 <77091365+KarthikeyaSarma30@users.noreply.github.com>
2022-12-06 13:07:33 +0530

    Add files via upload
    Print Fibonacci series upto n values and return nth value in that series

---
+++ fibprint.py
@@ -0,0 +1,17 @@
+#Fibonacci Series
+
+def fibprint(n):
+    a,b=0,1
+
+    print(a,b,end=" ")
+    term=3
+    
+    while term<=n:
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
+        term+=1
+    return c #nth number in Fibonacci series
+print(fibprint(5)) #3
+print(fibprint(60))
